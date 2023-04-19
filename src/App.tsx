a3adda2dc2e6a87b4839d6422cd1fe3bc1fd345d
wagner-tanaka <wbtanaka@hotmail.com>
2023-04-19 11:17:18 +0900

    [DEV] Lib - React Query

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,37 @@ import './App.css';
 import {
   BrowserRouter, Navigate, Route, Routes,
 } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import routes from './utils/routes';
 import LoginPage from './pages/login/loginPage';
 
 function App() {
+  // const [queryClient] = useState(
+  //   new QueryClient({
+  //     defaultOptions: {
+  //       queries: {
+  //         retry: false,
+  //         refetchOnWindowFocus: false,
+  //       },
+  //       mutations: {
+  //         retry: false,
+  //       },
+  //     },
+  //   }),
+  // );
+
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to={routes.login} />} />
-        <Route path={routes.login} element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={routes.login} />} />
+          <Route path={routes.login} element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+
   );
 }
 
